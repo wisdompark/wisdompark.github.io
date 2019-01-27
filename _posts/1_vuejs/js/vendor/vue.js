35a9b167c0f56505defe9985b669cd9b07a4a200
wisdompark <ifyouwonder90@naver.com>
2019-01-27 23:18:46 +0900

    chapter01

---
+++ _posts/1_vuejs/js/vendor/vue.js
@@ -0,0 +1,15 @@
+//전역 컴포넌트 등록
+Vue.component('todo-footer', {
+  template : '<p>This is another global child component</p>'
+});
+//지역 컴포넌트 내용
+var cmp = {
+  template : '<div>This is another local child component</div>'
+};
+//첫번째 인스턴스
+new Vue({
+  el : '#app',
+  components:{
+    'todo-list' : cmp
+  }
+});
