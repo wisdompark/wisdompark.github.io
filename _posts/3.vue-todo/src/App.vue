9f3157b02bc7730dd928cb63e82d1fe3dea9646e
wisdompark <ifyouwonder90@naver.com>
2019-02-06 03:28:48 +0900

    markdown

--- _posts/3.vue-todo/src/App.vue
+++ _posts/3.vue-todo/src/App.vue
@@ -7,10 +7,10 @@
   </div>
 </template>
 <script>
-import TodoHeader from '/components/TodoHeader.vue'
-import TodoInput from '/components/TodoInput.vue'
-import TodoList from '/components/TodoList.vue'
-import TodoFooter from '/components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
 
 export default{
 //컴포넌트 등록
